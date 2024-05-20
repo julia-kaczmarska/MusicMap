263c0f339e24f383e7feb35f9e34eea2ce6e8f0f
julia-kaczmarska <julia.kaczmarska.01@gmail.com>
2024-05-20 17:32:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/musicmap/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.musicmap
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+import com.example.musicmap.ui.theme.MusicMapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Znajdź przycisk i ustaw onClickListener
+        val button: Button = findViewById(R.id.but1)
+        button.setOnClickListener {
+            // Utwórz intent, aby uruchomić MapsActivity
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/musicmap/MapActivity.kt
@@ -0,0 +1,92 @@
+package com.example.musicmap
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+import android.Manifest
+
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        // Pobierz SupportMapFragment i powiadomienie o gotowości mapy
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Dodaj marker w Sydney i przesuń tam kamerę
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+
+    private fun getDeviceLocation() {
+        try {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+                val locationResult: Task<Location> = fusedLocationClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Ustawienie pozycji kamery na bieżącą lokalizację użytkownika
+                        val lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude), 15f))
+                        }
+                    } else {
+                        // Ustawienia domyślne w przypadku niepowodzenia uzyskania lokalizacji
+                        mMap.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                        mMap.isMyLocationEnabled = true
+                        getDeviceLocation()
+                    }
+                } else {
+                    // Permission denied, show a message to the user
+                }
+                return
+            }
+        }
+    }
+}
+
